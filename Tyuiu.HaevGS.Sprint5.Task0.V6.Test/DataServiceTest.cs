65a2a543f22869f9aabde3a9a949230b6c78b343
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-17 09:32:49 +0500

    Add project files.

---
+++ Tyuiu.HaevGS.Sprint5.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.HaevGS.Sprint5.Task0.V6.Lib;
+using System.IO;
+
+namespace Tyuiu.HaevGS.Sprint5.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"D:\Studio code\Repos2\Tyuiu.HaevGS.Sprint5\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool filexists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, filexists);
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint5.Task0.V6/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.HaevGS.Sprint5.Task0.V6.Lib;
+
+int x = 3;
+
+DataService ds = new DataService();
+
+string res = ds.SaveToFileTextData(x);
+
+Console.WriteLine("Файл" + res);
