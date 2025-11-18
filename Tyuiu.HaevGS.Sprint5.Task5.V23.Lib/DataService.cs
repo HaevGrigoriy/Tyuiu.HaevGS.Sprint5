4a7c7b63ea8a5d82da19e3cdcbdfdade9ccd14bd
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-18 12:09:31 +0500

    Task 5

--- Tyuiu.HaevGS.Sprint5.Task5.V23.Lib/DataService.cs
+++ Tyuiu.HaevGS.Sprint5.Task5.V23.Lib/DataService.cs
@@ -6,36 +6,25 @@ namespace Tyuiu.HaevGS.Sprint5.Task5.V23.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            double res = 0;
             using (StreamReader reader = new StreamReader(path))
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                string text = File.ReadAllText(path).Replace('.', ',');
+                string[] strings = text.Split(' ');
+
+                double minNumber = double.MaxValue;
+
+                foreach (string str in strings)
                 {
-                if (Convert.ToDouble(line) % 1 != 0)
+                    if (double.TryParse(str.Trim(), out double number))
                     {
-                        res += Convert.ToDouble(line);
+                        if (number % 1 == 0 && number % 4 != 0)
+                        {
+                            minNumber = number;
+                        }
                     }
                 }
-                
-            return res;
-                //string text = File.ReadAllText(path).Replace('.', ',');
-                //string[] strings = text.Split(' ');
-
-                //double minNumber = double.MaxValue;
-
-                //foreach (string str in strings)
-                //{
-                //    if (double.TryParse(str.Trim(), out double number))
-                //    {
-                //        if ((number % 1) != 0)
-                //        {
-                //            minNumber = number;
-                //        }
-                //    }
-                //}
-                //return minNumber;
-            }   
+                return minNumber;
+            }
         }
     }
 }
