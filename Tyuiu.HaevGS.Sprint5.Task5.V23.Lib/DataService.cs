cde13b6f3a2ec95dcd830c685edfb8099db49206
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-11-18 11:47:30 +0500

    Task 5

--- Tyuiu.HaevGS.Sprint5.Task5.V23.Lib/DataService.cs
+++ Tyuiu.HaevGS.Sprint5.Task5.V23.Lib/DataService.cs
@@ -9,37 +9,37 @@ namespace Tyuiu.HaevGS.Sprint5.Task5.V23.Lib
             double res = 0;
             using (StreamReader reader = new StreamReader(path))
             {
-                string text = File.ReadAllText(path).Replace('.', ',');
-                string[] strings = text.Split(' ');
+                //string text = File.ReadAllText(path).Replace('.', ',');
+                //string[] strings = text.Split(' ');
 
-                double minNumber = double.MaxValue;
+                //double minNumber = double.MaxValue;
 
-                foreach (string str in strings)
-                {
-                    if (double.TryParse(str.Trim(), out double number))
-                    {
-                        if (number % 1 == 0 && number % 4 == 0)
-                        {
-                            minNumber = number;
-                        }
-                    }
-                }
-                return minNumber;
-                //    string line;
-                //    while ((line = reader.ReadLine()) != null)
+                //foreach (string str in strings)
+                //{
+                //    if (double.TryParse(str.Trim(), out double number))
                 //    {
-                //        double number = Convert.ToDouble(line);
-                //        if (number % 1 != 0)
+                //        if (number % 1 == 0 && number % 4 == 0)
                 //        {
-                //            for (int i = 0; i <= number; i++)
-                //            {
-                //                res = number;
-                //            }
+                //            minNumber = number;
                 //        }
                 //    }
                 //}
-                //return res;
+                //return minNumber;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    double number = Convert.ToDouble(line);
+                    if (number % 1 != 0)
+                    {
+                        for (int i = 0; i <= number; i++)
+                        {
+                            res = number;
+                        }
+                    }
+                }
             }
+            return res;
         }
+        
     }
 }
